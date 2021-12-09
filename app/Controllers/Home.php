40ee5409ff5546881fe97bf54c9022419ee1c85e
gaurav narula <gauravnarulain@gmail.com>
2021-12-09 11:06:06 +0530

    validation

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,21 +2,52 @@
 
 namespace App\Controllers;
 
+// single
+
 class Home extends BaseController
 {
     private $genre;
+    // private $validation;
+    public $category = [
+        'title'     => 'required',
+        'releasedate'     => 'required',
+        'status' => 'required'
+    ];
+
+    public $category_errors = [
+        'title' => [
+            'required'    => 'You must choose a title.',
+        ],
+        'releasedate'    => [
+            'required' => 'Release date is required.'
+        ],
+        'status'    => [
+            'required' => 'Status is required'
+        ],
+    ];
+
     function __construct() // magic methofs
     {
         $this->genre = new \App\Models\Genre();
+        // $this->validation =  \Config\Services::validation();
         helper('form');
     }
     public function index()
     {
+
         $data['genre'] = $this->genre->findAll();
         return view('welcome_message', $data);
     }
     public function store()
     {
+        //https://www.positronx.io/codeigniter-form-validation-tutorial-with-example/
+
+        if (!$this->validate($this->category)) {
+            echo view('welcome_message', [
+                'validation' => $this->validator
+            ]);
+        }
+
         $postArray = $this->request->getVar();
 
         $data = array(
@@ -24,6 +55,8 @@ class Home extends BaseController
             'created' => $postArray['releasedate'],
             'status' => $postArray['status']
         );
+
+
         $res = $this->genre->insert($data);
         if ($res)
             return redirect()->back()->with('success', 'data has been saved successfully');
@@ -37,7 +70,7 @@ class Home extends BaseController
             'created' => date('Y-m-d h:i:s'),
             'status' => 'Active'
         );
-        $res = $this->genre->update(9, $data);
+        $res = $this->genre->update(13, $data);
         if ($res)
             echo "Data updated successfully";
         else
@@ -46,7 +79,7 @@ class Home extends BaseController
 
     public function delete()
     {
-        $res = $this->genre->delete(9);
+        $res = $this->genre->delete(13);
         if ($res)
             echo "Data delete successfully";
         else
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,30 +1,49 @@
 <?php include('layouts/header.php') ?>
 <div class="row">
     <div class="col-12">
+        <?php $validation = \Config\Services::validation(); ?>
+        <form method="post" action="<?= base_url('create'); ?>">
 
-        <?= form_open('create'); ?>
+            <div class="form-group">
+                <lable>Movie Category</lable>
+                <input type="text" name="title" id="title" class="form-control" />
 
-        <div class="form-group">
-            <lable>Movie Category</lable>
-            <input type="text" name="title" id="title" class="form-control" />
-        </div>
-        <div class="form-group">
-            <lable>Release Date</lable>
-            <input type="date" name="releasedate" id="releasedate" class="form-control" />
-        </div>
-        <div class="form-group">
-            <lable>Publish Status</lable>
-            <select name="status" class="form-control">
-                <option>Active</option>
-                <option>Inactive</option>
-            </select>
-        </div>
-        <div class="">&nbsp;</div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary btn-sm" name="btnsave" value="Save" />
-        </div>
-        <div class="">&nbsp;</div>
-        <?= form_close(); ?>
+            </div>
+            <!-- Error -->
+            <?php if ($validation->getError('title')) { ?>
+                <div class='alert alert-danger'>
+                    <?= $error = $validation->getError('title'); ?>
+                </div>
+            <?php } ?>
+            <div class="form-group">
+                <lable>Release Date</lable>
+                <input type="date" name="releasedate" id="releasedate" class="form-control" />
+                <!-- Error -->
+                <?php if ($validation->getError('releasedate')) { ?>
+                    <div class='alert alert-danger'>
+                        <?= $error = $validation->getError('releasedate'); ?>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="form-group">
+                <lable>Publish Status</lable>
+                <select name="status" class="form-control">
+                    <option>Active</option>
+                    <option>Inactive</option>
+                </select>
+                <!-- Error -->
+                <?php if ($validation->getError('status')) { ?>
+                    <div class='alert alert-danger'>
+                        <?= $error = $validation->getError('status'); ?>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="">&nbsp;</div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary btn-sm" name="btnsave" value="Save" />
+            </div>
+            <div class="">&nbsp;</div>
+        </form>
     </div>
     <div class="col-12">
         <div class="list-group">
